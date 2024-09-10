711d83aa2f9aa38cfc8bfc4571a2d82fe0cd088d
Matheus Benediht <matheusbenediht@gmail.com>
2024-09-10 13:33:35 -0300

    Update: Iniciando telas de login e registro

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,10 +1,40 @@
-import React from "react";
+import {React, useState} from "react";
 import { View, Text } from "react-native";
+import { Background, Container, AreaInput, Input } from "./styles";
+import { TextInput } from "react-native-paper";
 
 export default function SignIn(){
+    
+    const [text, setText] = useState('')
+
     return(
-        <View>
-            <Text>Tela de Login</Text>
-        </View>
+        <Background>
+            <Container>
+                <AreaInput>
+                <TextInput 
+                        label="E-mail"
+                        value={text}
+                        mode={'outlined'}
+                        onChangeText={text => setText(text)}
+                        outlineColor="#1e40af"
+                        activeOutlineColor="#1e40af"
+                        style={{
+                          
+                        }}
+                    />
+                <TextInput 
+                        label="Password"
+                        secureTextEntry
+                        mode={'outlined'}
+                        onChangeText={text => setText(text)}
+                        outlineColor="#1e40af"
+                        activeOutlineColor="#1e40af"
+                        style={{
+
+                        }}
+                    />
+                </AreaInput>
+            </Container>
+        </Background>
     )
 }
---
+++ frontend/src/pages/SignIn/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components/native";
+export const Background = styled.View`
+    flex: 1;
+    background-color: #F0F4FF;
+`
+
+export const Container = styled.KeyboardAvoidingView`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+  `
+
+export const AreaInput = styled.View`
+  flex-direction: column;
+  width: 80%;
+  gap: 10vh
+  `
+
